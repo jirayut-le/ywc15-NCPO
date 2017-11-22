f02cc211cd52e2eacbbe768492fbe62dce9bf357
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-11-22 17:19:19 +0700

    Add Logo and menu icon

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="navbar">
+        <div class="logo">
+            <img src="../assets/Logo.png">
+        </div>
+        <div class="menu">
+            ☰
+        </div>
+    </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+.navbar {
+    position: relative;
+    z-index: 5;
+    top: 0;
+    margin: 50px;
+
+    .logo {
+        float: left;
+        img {
+            max-width:50px;
+            max-height:50px;
+            width: auto;
+            height: auto;
+        }
+    }
+
+    .menu {
+        float: right;
+        font-size: 35px;
+        color: #fff;
+    }
+}
+
+</style>
