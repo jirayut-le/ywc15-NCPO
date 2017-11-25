0434f82ff95b3845d02b25123bc232e74c286eb1
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-11-25 16:02:42 +0700

    Change default font

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,10 @@ export default {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Mitr', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
 }
 </style>
