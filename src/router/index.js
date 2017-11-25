9c86337713ec32e3fbb256a28c8dc31fff03cc25
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-11-26 02:09:40 +0700

    Add path router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import PolicyAll from '@/components/PolicyAll'
 import PolicyNextPage from '@/components/PolicyNextPage'
+import PolicyDetail from '@/components/PolicyDetail'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/next',
       name: 'PolicyNextPage',
       component: PolicyNextPage
+    },
+    {
+      path: '/detail',
+      name: 'PolicyDetail',
+      component: PolicyDetail
     }
   ]
 })
