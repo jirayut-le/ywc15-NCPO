1e9292eb7cd3942b273c80cf2ba65022edb5c8cb
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-11-26 02:08:59 +0700

    Add page detail of each policy

---
+++ src/components/PolicyDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="policy_detail">
+        detail
+        <div class="columns">
+            <div class="column is-one-third"></div>
+            <div class="column"></div>
+        </div>
+
+    </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
