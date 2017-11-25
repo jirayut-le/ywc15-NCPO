bd0e157bae4a71112e88edeea60be9d64ae1707a
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-11-25 18:48:17 +0700

    Add policy components

---
+++ src/components/PolicyAll.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="policy_all">
+        <div class="columns">
+            <div class="column is-two-fifths">
+                <PolicyCard></PolicyCard>
+            </div>
+            <div class="column">
+                <PolicyCard></PolicyCard>
+                <PolicyCard></PolicyCard>
+            </div>
+            <div class="column" id="down">
+                <PolicyCard></PolicyCard>
+                <PolicyCard></PolicyCard>
+            </div>
+            <div class="column">
+                <PolicyCard></PolicyCard>
+                <PolicyCard></PolicyCard>
+            </div>
+        </div>
+        <router-link :to="{ name: 'PolicyNextPage'}">Next</router-link>
+    </div>
+  
+</template>
+
+<script>
+import PolicyCard from './PolicyCard'
+export default {
+    components: {
+        PolicyCard
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+#policy_all {
+    margin-top: 30px;
+}
+
+#down {
+    margin-top: 25px;
+}
+
+</style>
---
+++ src/components/PolicyCard.vue
@@ -0,0 +1,58 @@
+<template>
+<div id="policy_card">
+    <div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <!-- <figure class="image is-48x48">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+        </figure> -->
+      </div>
+      <div class="media-content">
+        <p class="has-text-left">Title</p>
+      </div>
+    </div>
+
+    <!-- <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris.
+    </div> -->
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="scss" scoped>
+
+.card {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    background-color: #F7F7F7;
+}
+
+#policy_card {
+    margin-bottom: 20px;
+}
+
+.card-content {
+    padding: 0.5rem;
+    padding-bottom: 50px;
+    margin-left: -12px;
+}
+
+.media-content {
+    color: #4A4A4A;
+    font-weight: 400;
+    font-size: 15px;
+}
+
+</style>
