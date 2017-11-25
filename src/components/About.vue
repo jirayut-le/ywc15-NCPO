ff7a464fac349b4ed8b8d0bb399efac35ac39a29
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-11-26 03:38:36 +0700

    Adding detail list of army

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,7 +10,15 @@
             </div>
         </div>
         <div class="army_list">
-
+            <h1> {{ title_army }}</h1>
+            <div class="columns">
+                <div class="column" v-for="logo in logos">
+                    <figure class="image is-96x96" >
+                        <img :src="getSrc(logo.name)" alt="Placeholder image">
+                        <div> {{ logo.des }}</div>
+                    </figure>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -21,13 +29,20 @@ export default {
       return {
           header: "คณะรักษาความสงบแห่งชาติ",
           description: 'หรือ คสช. (อังกฤษ: National Council for Peace and Order (NCPO) เดิมใช้ชื่อ National Peace and Order Maintaining Council (NPOMC) ) เป็นคณะผู้ยึดอำนาจการปกครอง โดยรัฐประหารยึดอำนาจการปกครอง เมื่อวันที่ 22 พฤษภาคม พ.ศ. 2557 ตั้งแต่เวลา 16:30 น. หลังเมื่อวันที่ 20 พฤษภาคม ประกาศกฎอัยการศึกทั่วราชอาณาจักร พร้อมทั้งจัดตั้งกองอำนวยการรักษาความสงบเรียบร้อย (กอ.รส.) ได้สองวัน และมีพลเอก ประยุทธ์ จันทร์โอชา เป็นหัวหน้าคณะ',
-          logo: [   { img: "", des: "กองบัญชาการกองทัพไทย" }, 
-                    { img: "", des: "กองทัพบก" },
-                    { img: "", des: "กองทัพเรือ"},
-                    { img: "", des: "กองทัพอากาศ"},
-                    { img: "", des: "ศูนย์บรรเทาสาธารณภัย กองทัพไทย"} ]
+          logos: [  { name: "army1", img: "../assets/army/army1.png", des: "กองบัญชาการกองทัพไทย" }, 
+                    {  name: "army2",img: "../assets/army/army2.png", des: "กองทัพบก" },
+                    {  name: "army3",img: "../assets/army/army3.png", des: "กองทัพเรือ"},
+                    {  name: "army4",img: "../assets/army/army4.png", des: "กองทัพอากาศ"},
+                    {  name: "army5",img: "../assets/army/army5.png", des: "ศูนย์บรรเทาสาธารณภัย กองทัพไทย"} ],
+          title_army: "เครื่องหมายของส่วนราชการที่เข้าร่วม"
       } 
-  }
+  },
+  methods: {
+    getSrc(name) {
+        var images = require.context('../assets/army/', false, /\.png$/);
+        return images('./' + name + ".png")
+    }
+}
 }
 </script>
 
@@ -47,10 +62,7 @@ export default {
 
     #text {
         margin-left: 20px;
-        h1 {
-            font-weight: 500;
-            font-size: 30px;
-        }
+        
 
         h4 {
             margin-top: 5px;
@@ -60,4 +72,25 @@ export default {
     }
 }
 
+.army_list {
+    padding: 100px 200px 100px 200px;
+
+    .columns {
+        margin-bottom: 40px;
+    }
+
+    h1 {
+        margin-bottom: 40px;
+    }
+
+    img {
+        margin-bottom: 20px;
+        border-radius: 100px;
+    }
+}
+
+h1 {
+    font-weight: 500;
+    font-size: 30px;
+}
 </style>
