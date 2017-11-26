4135995912a5e133f8e1a129c568ba8f1e1505cd
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-11-26 12:20:36 +0700

    Send data to props components

--- src/components/PolicyAll.vue
+++ src/components/PolicyAll.vue
@@ -4,17 +4,17 @@
             <div class="column is-two-fifths">
                 <PolicyHighlight></PolicyHighlight>
             </div>
-            <div class="column">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+            <div class="column" id="up">
+                <PolicyCard title='พัฒนาการจัดการศึกษาจังหวัดชายแดนภาคใต้' type='การศึกษา' icon='edu' img='1'></PolicyCard>
+                <PolicyCard title='ส่งเสริมให้ผู้มีรายได้น้อยมีที่อยู่อาศัย' type='เศรษฐกิจ' icon='econ' img='2'></PolicyCard>
             </div>
             <div class="column" id="down">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+                <PolicyCard title='การแก้ไขปัญหาความขัดแย้งในการจัดการที่ดินป่าไม้' type='สิ่งแวดล้อม' icon='eco' img='3'></PolicyCard>
+                <PolicyCard title='ส่งเสริมการกีฬาเพื่อสุขภาพ' type='สาธารณสุข' icon='health' img='4'></PolicyCard>
             </div>
-            <div class="column">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+            <div class="column" id="up">
+                <PolicyCard title='การกำหนดการแก้ไขปัญหาขยะมูลฝอย' type='สิ่งแวดล้อม' icon='eco' img='5'></PolicyCard>
+                <PolicyCard title='การพัฒนาแหล่งน้ำ (แก้ไขปัญหาอุทกภัย และภัยแล้ง)' type='สิ่งแวดล้อม' icon='eco' img='6'></PolicyCard>
             </div>
         </div>
         <a class="button is-light" disabled>
@@ -46,6 +46,35 @@ export default {
     margin-bottom: 0;
 }
 
+#up {
+    animation: move-down 0.5s ease-in-out;
+}
+
+#down {
+    animation: move-up 0.5s ease-in-out;
+}
+
+@keyframes move-down {
+    from {
+        transform: translateY(-20px);
+    }
+
+    to {
+        transform: translateY(0);
+    }
+}
+
+@keyframes move-up {
+
+    from {
+        transform: translateY(20px);
+    }
+
+    to {
+        transform: translateY(0);
+    }
+    
+}
 #policy_all {
     margin-top: 30px;
 }
--- src/components/PolicyNextPage.vue
+++ src/components/PolicyNextPage.vue
@@ -1,25 +1,25 @@
 <template>
     <div id="policy_next">
         <div class="columns">
-            <div class="column">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+            <div class="column" id="up">
+                <PolicyCard title='ป้องกันและแก้ไขปัญหาการเกิดอุบัติเหตุในการจราจร' type='สาธารณสุข' icon='health' img='7'></PolicyCard>
+                <PolicyCard title='ขยายการก่อสร้างทางด่วนพิเศษ หรือที่เรารู้จักกันคือมอเตอร์เวย์' type='เศรษฐกิจ' icon='econ' img='8'></PolicyCard>
             </div>
             <div class="column" id="down">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+                <PolicyCard title='ส่งเสริมการกีฬาเพื่อสุขภาพ' type='สาธารณสุข' icon='health' img='4'></PolicyCard>
+                <PolicyCard title='พัฒนาการจัดการศึกษาจังหวัดชายแดนภาคใต้' type='การศึกษา' icon='edu' img='1'></PolicyCard>
             </div>
-            <div class="column">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+            <div class="column" id="up">
+                <PolicyCard title='การพัฒนาแหล่งน้ำ (แก้ไขปัญหาอุทกภัย และภัยแล้ง)' type='สิ่งแวดล้อม' icon='eco' img='6'></PolicyCard>
+                <PolicyCard title='ส่งเสริมให้ผู้มีรายได้น้อยมีที่อยู่อาศัย' type='เศรษฐกิจ' icon='econ' img='2'></PolicyCard>
             </div>
             <div class="column" id="down">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+                <PolicyCard title='การแก้ไขปัญหาความขัดแย้งในการจัดการที่ดินป่าไม้' type='สิ่งแวดล้อม' icon='eco' img='3'></PolicyCard>
+                <PolicyCard title='การกำหนดการแก้ไขปัญหาขยะมูลฝอย' type='สิ่งแวดล้อม' icon='eco' img='5'></PolicyCard>
             </div>
-            <div class="column">
-                <PolicyCard></PolicyCard>
-                <PolicyCard></PolicyCard>
+            <div class="column" id="up">
+                <PolicyCard title='ขยายการก่อสร้างทางด่วนพิเศษ หรือที่เรารู้จักกันคือมอเตอร์เวย์' type='เศรษฐกิจ' icon='econ' img='8'></PolicyCard>
+                <PolicyCard title='ส่งเสริมการกีฬาเพื่อสุขภาพ' type='สาธารณสุข' icon='health' img='4'></PolicyCard>
             </div>
         </div>
         <router-link :to="{ name: 'PolicyAll'}" class="next-btn">
@@ -53,6 +53,36 @@ export default {
 
 #down {
     margin-top: 25px;
+}
+
+#up {
+    animation: move-down 0.5s ease-in-out;
+}
+
+#down {
+    animation: move-up 0.5s ease-in-out;
+}
+
+@keyframes move-down {
+    from {
+        transform: translateY(-20px);
+    }
+
+    to {
+        transform: translateY(0);
+    }
+}
+
+@keyframes move-up {
+
+    from {
+        transform: translateY(20px);
+    }
+
+    to {
+        transform: translateY(0);
+    }
+    
 }
 
 .columns {
